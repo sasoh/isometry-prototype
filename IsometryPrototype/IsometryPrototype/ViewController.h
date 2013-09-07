65520a2b280b13d82d1978092f477c9a97e112fe
sasoh <lxndrppv@gmail.com>
2013-09-07 14:31:16 +0300

    [+] adding Xcode project files to repository

---
+++ IsometryPrototype/IsometryPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IsometryPrototype
+//
+//  Created by Alexander Popov on 9/7/13.
+//  Copyright (c) 2013 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ IsometryPrototype/IsometryPrototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IsometryPrototype
+//
+//  Created by Alexander Popov on 9/7/13.
+//  Copyright (c) 2013 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
