09484915e0cffb2e5a5c19686fb235dccb0373e2
sasoh <lxndrppv@gmail.com>
2013-09-07 15:42:15 +0300

    [+] simple non-square grid shown in carthesian coordinate system [+] simple touch location (not clamped to map dimensions)

--- IsometryPrototype/IsometryPrototype/ViewController.m
+++ IsometryPrototype/IsometryPrototype/ViewController.m
@@ -8,7 +8,30 @@
 
 #import "ViewController.h"
 
+const int mapColumns    = 10;
+const int mapRows       = 8;
+const int map[mapRows][mapColumns] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+};
+const CGFloat tileSize = 30.0;
+
 @interface ViewController ()
+
+- (void)drawArrayMap;
+- (void)drawArrayTile:(int)type inView:(UIView *)container atPoint:(CGPoint)position;
+- (void)addInputInView:(UIView *)targetView;
+- (void)handleInputFrom:(UIGestureRecognizer *)recognizer;
+
+// screen to grid
+- (CGPoint)gridFromScreen:(CGPoint)position;
+- (CGPoint)screenFromGridX:(int)column andY:(int)row;
 
 @end
 
@@ -24,6 +47,89 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    
+    [statusLabel setText:@""];
+    
+    [self drawArrayMap];
+    // add gesture recognizer to detect taps
+    [self addInputInView:containerView];
+
+    [super viewWillAppear:animated];
+
+}
+
+- (void)drawArrayMap
+{
+    
+    for (int row = 0; row < mapRows; ++row) {
+        for (int column = 0; column < mapColumns; ++column) {
+            CGPoint tilePosition = CGPointZero;
+            tilePosition.x = column * tileSize;
+            tilePosition.y = row * tileSize;
+            [self drawArrayTile:map[row][column] inView:containerView atPoint:tilePosition];
+        }
+    }
+    
+    
+}
+
+- (void)drawArrayTile:(int)type inView:(UIView *)container atPoint:(CGPoint)position
+{
+    
+    UIView *tileView = [[UIView alloc] initWithFrame:CGRectMake(position.x, position.y, tileSize, tileSize)];
+    UIColor *tileColor = nil;
+    if (type == 0) {
+        tileColor = [UIColor greenColor];
+    } else {
+        tileColor = [UIColor yellowColor];
+    }
+    [tileView setBackgroundColor:tileColor];
+    [container addSubview:tileView];
+    
+}
+
+- (void)addInputInView:(UIView *)targetView
+{
+    
+    UITapGestureRecognizer *recognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleInputFrom:)];
+    [targetView addGestureRecognizer:recognizer];
+    
+}
+
+- (void)handleInputFrom:(UIGestureRecognizer *)recognizer
+{
+    
+    CGPoint location = [recognizer locationInView:containerView];
+    
+    CGPoint gridLocation = [self gridFromScreen:location];
+    
+    NSString *str = [NSString stringWithFormat:@"Actual position: %@\nGrid position: %@\nSnapped screen position: %@", NSStringFromCGPoint(location), NSStringFromCGPoint(gridLocation), NSStringFromCGPoint([self screenFromGridX:gridLocation.x andY:gridLocation.y])];
+    [statusLabel setText:str];
+    
+}
+
+- (CGPoint)gridFromScreen:(CGPoint)position
+{
+    
+    CGPoint result = CGPointZero;
+    result.x = floorf(position.x / tileSize);
+    result.y = floorf(position.y / tileSize);
+    return result;
+    
+}
+
+- (CGPoint)screenFromGridX:(int)column andY:(int)row
+{
+    
+    CGPoint result = CGPointZero;
+    result.x = column * tileSize;
+    result.y = row * tileSize;
+    return result;
+    
 }
 
 @end
--- README.md
+++ README.md
@@ -4,8 +4,8 @@ isometry-prototype
 I'll be using this project to get a better grasp of the 2D isometric projection.
 
 To do:
-- [ ] implement a simple 2d grid
-- [ ] display grid in normal carthesian coordinate system
+- [x] implement a simple 2d grid
+- [x] display grid in normal carthesian coordinate system
 - [ ] display grid in isometric projection
 - [ ] detect touch location
 - [ ] convert touch locations from screen space to isometric & vice versa
